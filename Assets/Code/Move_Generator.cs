654a7f6fdcddf0c508f22ca0581419bd15f206df
Daniel Sedlov <mr.sedlov@gmail.com>
2021-01-07 12:50:50 -0700

    Prep for Valid Move Generator
    Groundwork

---
+++ Assets/Code/Move_Generator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExtraChessStructures;
+
+public class Move_Generator : MonoBehaviour
+{
+    public void Generate(Transform[,] board, bool LookForBlackPieces)
+    {
+        List<Move> moves = new List<Move>();
+        foreach (Transform piece in board)
+        {
+            if (piece != null)
+            {
+                Chess_Piece chess_Piece = piece.GetComponent<Chess_Piece>();
+                if (chess_Piece.GetIsBlack() == LookForBlackPieces)
+                {
+                    foreach (Move move in chess_Piece.GenerateAllValidMoves())
+                    {
+                        moves.Add(move);
+                    }
+                }
+            }
+        }
+    }
+}
+
